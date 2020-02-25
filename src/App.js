42bd201cd4f71c935beac0fc5b80348ddc218825
Vishvak Seenichamy <vishvak365@gmail.com>
2020-02-25 16:09:42 -0500

    Implemented local image gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import MenuAppBar from './components/appbar/appbar.js';
+import ImGallery from './components/gallery.js';
+let images = {};
+async function addImages() {
+  console.log("adding images");
+  for (let i = 0; i < 1; i++) {
+    images[i] = {
+      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+      caption: i + 1 + "Test",
+    }
+  }
+}
 
 function App() {
+  addImages();
   return (
 
     <div className="App">
       <MenuAppBar></MenuAppBar>
+      <ImGallery></ImGallery>
     </div>
   );
 }
