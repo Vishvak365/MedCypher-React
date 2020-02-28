23b8784a94ca3d9856f91e3feb535ceac837beb3
zaneZudans <zpzane@ufl.edu>
2020-02-28 13:25:30 -0500

    created galleryOutput Array to act as temp for images so filtering doesn't permanently pare down images.

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,25 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Gallery from 'react-grid-gallery';//https://www.npmjs.com/package/react-grid-gallery
 import testImage from '../images/testimages/science-in-hd-4pM4nhHyo9M-unsplash.jpg'
+
+
 let images = [];
-<<<<<<< HEAD
 let galleryOutput=[]; //This is for displaying what is output to gallery.
 async function addImages() {
     console.log("adding images");
     for (let i = 0; i < 67; i++) {
-=======
-var doneLoading = false;
-function getData(data) {
-    console.log("Parsing through data");
-    for (let i = 0; i < data.length; i++) {
->>>>>>> 1ad756ce3a0639cfe7f65ac0541a8bcc9c8223a2
         images[i] = {
-            src: data[i].ImageUri,
-            thumbnail: data[i].ImageUri,
-            thumbnailWidth: Number(data[i].imageWidth),
-            thumbnailHeight: Number(data[i].imageHeight),
-            caption: data[i].captions,
-            tags: [{value: data[i].tags}]
+            src: "http://www.gannett-cdn.com/-mm-/d415e49dbee251019f24a0ea55f95947a2bb1072/c=29-0-425-298&r=x404&c=534x401/local/-/media/2016/01/02/DetroitFreePress/DetroitFreePress/635873333613566290-surgery-ThinkstockPhotos-470454993.jpg",
+            thumbnail: "http://www.gannett-cdn.com/-mm-/d415e49dbee251019f24a0ea55f95947a2bb1072/c=29-0-425-298&r=x404&c=534x401/local/-/media/2016/01/02/DetroitFreePress/DetroitFreePress/635873333613566290-surgery-ThinkstockPhotos-470454993.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            tags: [{ value: "Scalpel" }, { value: "Stabby Thing" }, { value: "Pointy" }, {value: "cutty Thing"}],
+            caption: "This tool cuts people open"
         }
     }
     //Added an element with an additional tag for testing if the filtering works.
@@ -31,7 +26,6 @@ function getData(data) {
             tags: [{ value: "Scalpel" }, { value: "Stabby Thing" }, { value: "Pointy" }, {value: "cutty Thing"}, {value: "Very Sharp Knife"}],
             caption: "This tool cuts people open"
     }
-  doneLoading = true;
 };
 
 //Filters image vector (defined above) by tag
@@ -60,30 +54,8 @@ async function filterImages(searchFilter){
 function ImGallery(props) {
     addImages();
     filterImages(props.search);
-function ImGallery() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState();
-    if (isLoading) {
-        fetch('https://medcypher.azurewebsites.net/api/data/Dr.%20Mohammad')
-            .then(res => res.json())
-            .then(response => {
-                setData(response);
-                getData(response);
-                console.log(data);
-                if (doneLoading)
-                    setIsLoading(false);
-            }).catch(e => { console.log(e) });
-    }
     return (
-<<<<<<< HEAD
         <Gallery images={galleryOutput} />
-=======
-        <div>
-            {isLoading ? <p>Loading Data</p> : <Gallery images={images} />}
-
-        </div>
->>>>>>> 1ad756ce3a0639cfe7f65ac0541a8bcc9c8223a2
     );
-
 }
 export default ImGallery;
