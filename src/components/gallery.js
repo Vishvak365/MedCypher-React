f40ded918d6eebc7a2982decac7907da9cb97e49
Vishvak Seenichamy <vishvak365@gmail.com>
2020-02-28 16:31:50 -0500

    Updated to new API specs

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,32 +1,34 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 import Gallery from 'react-grid-gallery';//https://www.npmjs.com/package/react-grid-gallery
-import testImage from '../images/testimages/science-in-hd-4pM4nhHyo9M-unsplash.jpg'
-
-
 let images = [];
-let galleryOutput=[]; //This is for displaying what is output to gallery.
-var doneLoading=false;
+let galleryOutput = []; //This is for displaying what is output to gallery.
+var doneLoading = false;
 function getData(data) {
     console.log("Parsing through data");
     for (let i = 0; i < data.length; i++) {
         images[i] = {
-            src: data[i].ImageUri,
-            thumbnail: data[i].ImageUri,
-            thumbnailWidth: Number(data[i].imageWidth),
-            thumbnailHeight: Number(data[i].imageHeight),
+            src: data[i].imageUri,
+            thumbnail: data[i].imageUri,
+            thumbnailWidth: data[i].imageWidth,
+            thumbnailHeight: data[i].imageHeight,
             caption: data[i].captions,
-            tags: [{ value: data[i].tags }]
+            tags: [],
+        };
+        //console.log(images[0].tags);
+        for (let j = 0; j < data[i].tags.length; j++) {
+            images[i].tags[j] = { value: data[i].tags[j] };
         }
     }
+
     //Added an element with an additional tag for testing if the filtering works.
-    images[data.length] = {
-        src: "http://www.gannett-cdn.com/-mm-/d415e49dbee251019f24a0ea55f95947a2bb1072/c=29-0-425-298&r=x404&c=534x401/local/-/media/2016/01/02/DetroitFreePress/DetroitFreePress/635873333613566290-surgery-ThinkstockPhotos-470454993.jpg",
-        thumbnail: "http://www.gannett-cdn.com/-mm-/d415e49dbee251019f24a0ea55f95947a2bb1072/c=29-0-425-298&r=x404&c=534x401/local/-/media/2016/01/02/DetroitFreePress/DetroitFreePress/635873333613566290-surgery-ThinkstockPhotos-470454993.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        tags: [{ value: "Scalpel" }, { value: "Stabby Thing" }, { value: "Pointy" }, { value: "cutty Thing" }, { value: "Very Sharp Knife" }],
-        caption: "This tool cuts people open"
-    }
+    // images[data.length] = {
+    //     src: "http://www.gannett-cdn.com/-mm-/d415e49dbee251019f24a0ea55f95947a2bb1072/c=29-0-425-298&r=x404&c=534x401/local/-/media/2016/01/02/DetroitFreePress/DetroitFreePress/635873333613566290-surgery-ThinkstockPhotos-470454993.jpg",
+    //     thumbnail: "http://www.gannett-cdn.com/-mm-/d415e49dbee251019f24a0ea55f95947a2bb1072/c=29-0-425-298&r=x404&c=534x401/local/-/media/2016/01/02/DetroitFreePress/DetroitFreePress/635873333613566290-surgery-ThinkstockPhotos-470454993.jpg",
+    //     thumbnailWidth: 320,
+    //     thumbnailHeight: 212,
+    //     tags: [{ value: "Scalpel" }, { value: "Stabby Thing" }, { value: "Pointy" }, { value: "cutty Thing" }, { value: "Very Sharp Knife" }],
+    //     caption: "This tool cuts people open"
+    // }
     doneLoading = true;
 };
 
@@ -36,10 +38,10 @@ async function filterImages(searchFilter) {
     //let tagsTogether=''; This string will concatenate all the tags together so that using indexOf will return
     // true if the search input contains any of the tags. This will be used to determine
     // which images are kept by the .filter() method.
-    galleryOutput=images.filter((image) =>{
-        var tagsTogether=image.tags[0].value+' '; //Reinitialzing to first tag
-        for(let i=0;i<image.tags.length;i++){
-            tagsTogether=tagsTogether.concat(image.tags[i].value+' ');
+    galleryOutput = images.filter((image) => {
+        var tagsTogether = image.tags[0].value + ' '; //Reinitialzing to first tag
+        for (let i = 0; i < image.tags.length; i++) {
+            tagsTogether = tagsTogether.concat(image.tags[i].value + ' ');
         }                                                                           //Separating tags by spaces so as to avoid
         //erroneously true positive returns.
         if (tagsTogether.toLowerCase().indexOf(' ' + searchFilter.toLowerCase()) >= 0) return true;
@@ -60,16 +62,16 @@ function ImGallery(props) {
         fetch('https://medcypher.azurewebsites.net/api/data/Dr.%20Mohammad')
             .then(res => res.json()) // converts to json
             .then(response => {
-                setData(response); //Set the data hook with API respnose data
-                getData(response); //Populates the image array
+                setData(response.data); //Set the data hook with API respnose data
+                getData(response.data); //Populates the image array
                 if (doneLoading)
                     setIsLoading(false); //once done loading image array, then set the loading hook with false
             }).catch(e => { console.log(e) });
     }
-    filterImages(props.search); //Filtering image array based on search input
+    //filterImages(props.search); //Filtering image array based on search input
     return (
         <div>
-            {isLoading ? <p>Loading Data</p> : <Gallery images={galleryOutput} />}
+            {isLoading ? <p>Loading Data</p> : <Gallery images={images} />}
         </div>
     );
 }
