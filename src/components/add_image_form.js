4fc59b47a6fd15d5d70b39b16b99acc04d4f2aa2
Vishvak Seenichamy <vishvak365@gmail.com>
2020-02-28 15:28:13 -0500

    Input Tags basic functionality

--- src/components/add_image_form.js
+++ src/components/add_image_form.js
@@ -1,22 +1,32 @@
 import { Form } from 'semantic-ui-react'
-import React from 'react';
+import React, { useState } from 'react';
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
 
 export default function AddForm() {
+    const handleTags = (tag) => {
+        setTags(tag);
+    }
+    const [tags, setTags] = useState([]); //For use in filtering ImgGallery
     return (
-        <Form.Group widths='equal'>
-            <Form.Input
-                fluid
-                id='form-subcomponent-shorthand-input-first-name'
-                label='Doctor Name'
-                placeholder='Dr. Lastname'
-            />
-            <br></br>
-            <Form.Input
-                fluid
-                id='form-subcomponent-shorthand-input-last-name'
-                label='Last name'
-                placeholder='Last name'
-            />
-        </Form.Group>
+        <div>
+            <Form.Group widths='equal'>
+                <Form.Input
+                    fluid
+                    id='form-subcomponent-shorthand-input-first-name'
+                    label='Doctor Name'
+                    placeholder='Dr. Lastname'
+                />
+                <br></br>
+                <Form.Input
+                    fluid
+                    id='form-subcomponent-shorthand-input-last-name'
+                    label='Last name'
+                    placeholder='Last name'
+                />
+            </Form.Group>
+            <TagsInput value={tags} onChange={handleTags} />
+        </div>
+
     );
 };
